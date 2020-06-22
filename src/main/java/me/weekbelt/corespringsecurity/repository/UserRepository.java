c7f20cfbbe2c6363fd5f44bc1ccbccfa9b34fecb
김주혁 <vfrvfr4207@gmail.com>
2020-06-22 14:08:04 +0900

    Create CustomerUserDetailsService

--- src/main/java/me/weekbelt/corespringsecurity/repository/UserRepository.java
+++ src/main/java/me/weekbelt/corespringsecurity/repository/UserRepository.java
@@ -3,5 +3,9 @@ package me.weekbelt.corespringsecurity.repository;
 import me.weekbelt.corespringsecurity.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByUsername(String username);
 }
---
+++ src/main/java/me/weekbelt/corespringsecurity/security/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package me.weekbelt.corespringsecurity.security.service;
+
+import lombok.RequiredArgsConstructor;
+import me.weekbelt.corespringsecurity.domain.Account;
+import me.weekbelt.corespringsecurity.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("UserNotFountException"));
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(account.getRole()));
+
+        return new AccountContext(account, roles);
+    }
+}
